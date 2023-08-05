84f7be67cbf1c1af2cd23fb4d905f9bdb824ea0b
Colby Jennings <bcjennings@missouristate.edu>
2023-08-05 16:46:54 -0500

    image and bg updates
    images and bgs

---
+++ js/10print.js
@@ -0,0 +1,31 @@
+let spacing = 20; // Define the spacing between lines
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); // Make the canvas fill the browser window
+  background(12); // Set a light gray background
+  stroke(18, 20, 18); // Set the line color to a darker gray
+  strokeWeight(3); // Set the line thickness
+  noLoop(); // Run the drawing once without looping
+}
+
+function draw() {
+  for (let x = 0; x < width; x += spacing) {
+    for (let y = 0; y < height; y += spacing) {
+      push(); // Save the current drawing style
+      translate(x, y); // Move to the current grid position
+      let choice = floor(random(2)); // Randomly pick 0 or 1
+      if (choice === 0) {
+        line(0, 0, spacing, spacing); // Draw a "\"
+      } else {
+        line(spacing, 0, 0, spacing); // Draw a "/"
+      }
+      pop(); // Restore the drawing style
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(12);
+  draw();
+}
