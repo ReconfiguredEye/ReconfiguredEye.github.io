ebd1097debe4ad52765e82f1a77529499337bae6
Colby Jennings <bcjennings@missouristate.edu>
2025-02-17 16:42:19 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@ const feeds = [
     "https://s1-bos.liveatc.net/opla_atis?nocache=2025012621150324346",
     "https://s1-fmt2.liveatc.net/ltfj2?nocache=2025012621445312087",
     "https://s1-bos.liveatc.net/vhhh5?nocache=2025012621484253371",
-    "https://s1-bos.liveatc.net/sazn_twr?nocache=2025012621502528156"
+    "https://s1-bos.liveatc.net/sazn_twr?nocache=2025012621502528156",
+    "https://s1-bos.liveatc.net/ksgf2_app?nocache=2025021722362081063",
+    "https://s1-bos.liveatc.net/epkk_app?nocache=2025021722393194100"
 
 ];
 
@@ -21,7 +23,7 @@ let audioPlayers = [];
 // Helper function to get three random feeds
 function getRandomFeeds() {
     const shuffled = feeds.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, 3);
+    return shuffled.slice(0, 4);
 }
 
 // Play the selected feeds
